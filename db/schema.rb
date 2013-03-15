c2a744fc13e9a8407cd2072bff594a111a4848e5
Adrian Peterson Co <adrianpco@gmail.com>
2013-03-15 22:34:20 +0800

    Habol si schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130315140641) do
+ActiveRecord::Schema.define(:version => 20130315142752) do
 
   create_table "pledges", :force => true do |t|
     t.string   "first_name"
@@ -24,6 +24,19 @@ ActiveRecord::Schema.define(:version => 20130315140641) do
     t.datetime "created_at",                           :null => false
     t.datetime "updated_at",                           :null => false
   end
+
+  create_table "rails_admin_histories", :force => true do |t|
+    t.text     "message"
+    t.string   "username"
+    t.integer  "item"
+    t.string   "table"
+    t.integer  "month",      :limit => 2
+    t.integer  "year",       :limit => 5
+    t.datetime "created_at",              :null => false
+    t.datetime "updated_at",              :null => false
+  end
+
+  add_index "rails_admin_histories", ["item", "table", "month", "year"], :name => "index_rails_admin_histories"
 
   create_table "scholars", :force => true do |t|
     t.string   "first_name"
