3b00a35db0087ebf22192b702d60ea21075261a8
Ken-Lauren Daganio <akosiken@gmail.com>
2013-03-16 15:28:57 +0800

    BAKIT KASI WALANG VALIDATION SA AMOUNT PETE?????

--- app/models/pledge.rb
+++ app/models/pledge.rb
@@ -2,7 +2,7 @@ class Pledge < ActiveRecord::Base
   attr_accessible :amount, :status, :first_name, :last_name, :email, :contact_number, :message, :scholar_id
 
   validates_presence_of :email, :amount
-  validates_numericality_of :amount
+  validates :amount, numericality: { greater_than: 1 } 
 
   scope :paid, where(status: 'Paid')
   scope :unpaid, where(status: 'Unpaid')
