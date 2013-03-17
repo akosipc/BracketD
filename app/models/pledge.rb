897590aaf2adb12927b210b6b614aace1a17cbae
Adrian Peterson Co <adrianpco@gmail.com>
2013-03-17 22:54:15 +0800

    Adds validations. Removes tracking of unpaid pledges

--- app/models/pledge.rb
+++ app/models/pledge.rb
@@ -2,10 +2,13 @@ class Pledge < ActiveRecord::Base
   attr_accessible :amount, :status, :first_name, :last_name, :email, :contact_number, :message, :scholar_id
 
   validates_presence_of :email, :amount
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
   validates :amount, numericality: { greater_than: 1 } 
 
   scope :paid, where(status: 'Paid')
   scope :unpaid, where(status: 'Unpaid')
 
   belongs_to :scholar
+
 end
+
